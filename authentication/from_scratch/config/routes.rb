0da45a80493b58724278aa94baa5e7bd779d46eb
Zeni <gustavo.zeni@pier.digital>
2024-05-15 09:10:12 -0300

    update

--- authentication/from_scratch/config/routes.rb
+++ authentication/from_scratch/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new', :as => 'login'
+  get 'register', to: 'users#new'
+  get 'articles/new' => 'articles#new', :as => 'new_article'
+  get 'users/new'
+  # root 'users#new'
   root "articles#index"
 
+  resources :users
+  resources :sessions
   resources :articles do
     resources :comments
   end
